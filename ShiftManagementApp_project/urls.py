24afa8c054aabc8d0ed8fe363d7a22b7b18f6e60
takumi <y.takumi4@gmail.com>
2022-11-19 00:47:28 +0900

    API認証URLを追加

--- ShiftManagementApp_project/urls.py
+++ ShiftManagementApp_project/urls.py
@@ -4,4 +4,5 @@ from django.urls import path,include
 urlpatterns = [
     path('', include('ShiftManagementApp.urls')),
     path('admin/', admin.site.urls),
+    path('api-auth/', include('rest_framework.urls')),
 ]
