82431f2ef4af943c75e034cb62b00360cefaa726
takumi <y.takumi4@gmail.com>
2022-11-19 00:36:12 +0900

    DjangoFilterBackendを使ってfilterするように変更

--- ShiftManagementApp/views.py
+++ ShiftManagementApp/views.py
@@ -1,6 +1,7 @@
 from rest_framework import viewsets
 from ShiftManagementApp.models import User,Shift
 from ShiftManagementApp.serializers import UserSerializer,ShiftSerializer
+from django_filters.rest_framework import DjangoFilterBackend
 
 class UserViewSet(viewsets.ModelViewSet):
     queryset = User.objects.all()
@@ -8,11 +9,6 @@ class UserViewSet(viewsets.ModelViewSet):
 
 class ShiftViewSet(viewsets.ModelViewSet):
     serializer_class = ShiftSerializer
-
-    def get_queryset(self):
-        queryset = Shift.objects.all().order_by('begin')
-        user_id = self.request.query_params.get('user_id')
-        if user_id is not None:
-            user_id = int(user_id)
-            queryset = queryset.filter(user=user_id)
-        return queryset
+    queryset = Shift.objects.all().order_by('begin')
+    filter_backends = [DjangoFilterBackend]
+    filterset_fields = ['date','user']
