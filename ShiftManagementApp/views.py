1561bbd72d39c2f694d6278ba88589ffa646dcc2
takumi <y.takumi4@gmail.com>
2022-11-19 18:29:19 +0900

    Shiftをcreateする時は、userをrequest.userで上書きする

--- ShiftManagementApp/views.py
+++ ShiftManagementApp/views.py
@@ -22,24 +22,13 @@ class ShiftViewSet(viewsets.ModelViewSet):
     serializer_class = ShiftSerializer
     filter_backends = (filters.DjangoFilterBackend,)
     filterset_class = ShiftFilter
+
+    def perform_create(self, serializer):
+        serializer.save(user=self.request.user)
     
     def get_queryset(self):
         if self.request.user.is_staff == True:
             return Shift.objects.all().order_by('begin')
         else:
-            return Shift.objects.filter(publish=True).order_by('begin') | Shift.objects.filter(user=self.request.user).order_by('begin')
-
-    """ 
-    def get_queryset(self):
-        
-       #クエリパラメータuserの有無で返すquerysetを変える
-        if self.request.query_params.get('user') is not None:
-
-             #クエリパラメータでuserのシフトを要求したユーザーが本人か、そうでないかで返すquertsetを変える
-            if self.request.user.id == int(self.request.query_params.get('user')):
-                return Shift.objects.filter(id=int(self.request.query_params.get('user'))).order_by('begin')
-            else:
-                return Shift.objects.filter(id=int(self.request.query_params.get('user')),publish=True).order_by('begin')
-        else:
-            return Shift.objects.all()
-    """
+            queryset = Shift.objects.filter(publish=True) | Shift.objects.filter(user=self.request.user)
+            return queryset.order_by('begin')
