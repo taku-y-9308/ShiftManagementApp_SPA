c7fd82d92dd33d5410aa570214b27fb5ac403d9d
takumi <y.takumi4@gmail.com>
2022-11-19 14:42:31 +0900

    取得するシフトのdateの範囲をフィルタリングできるようにする

--- ShiftManagementApp/views.py
+++ ShiftManagementApp/views.py
@@ -2,13 +2,24 @@ from rest_framework import viewsets
 from ShiftManagementApp.models import User,Shift
 from ShiftManagementApp.serializers import UserSerializer,ShiftSerializer
 from django_filters.rest_framework import DjangoFilterBackend
+from django_filters import rest_framework as filters 
+
 
 class UserViewSet(viewsets.ModelViewSet):
     queryset = User.objects.all()
     serializer_class = UserSerializer
 
+class ShiftFilter(filters.FilterSet):
+    date_gte = filters.DateFilter(field_name="date",lookup_expr="gte")
+    date_lte = filters.DateFilter(field_name="date",lookup_expr="lte")
+
+    class Meta:
+        model = Shift
+        fields = ['date_gte','date_lte','user']
+
+
 class ShiftViewSet(viewsets.ModelViewSet):
     serializer_class = ShiftSerializer
     queryset = Shift.objects.all().order_by('begin')
-    filter_backends = [DjangoFilterBackend]
-    filterset_fields = ['date','user']
+    filter_backends = (filters.DjangoFilterBackend,)
+    filterset_class = ShiftFilter
